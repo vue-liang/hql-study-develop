d305b8e0e1b75c8ab91e5091c7ea1d2c24d3139e
Vue-liang <2330983050@qq.com>
2025-10-27 20:47:25 +0800

    feat:测试I/O操作的API v1.0

--- spring-learn/src/test/java/com/liang/ApiTest.java
+++ spring-learn/src/test/java/com/liang/ApiTest.java
@@ -3,11 +3,52 @@ package com.liang;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class ApiTest {
     @Test
-    public void test01(){
-        System.out.println("测试代码评审");
-        System.out.println("测试代码评审1");
-        System.out.println("测试代码评审2");
+    public void test01() throws IOException {
+        File file = new File("./src/test/java/files/test.txt");
+        File dateFolder = new File("src/test/java/files");
+        if (!dateFolder.exists()) {
+            dateFolder.mkdirs();
+        }
+        try (FileWriter writer = new FileWriter(file)){
+            writer.write("你这么厉害？");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try(OutputStream os = new FileOutputStream(file,true)){
+            os.write("你真厉害".getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void sendPostRequest(String urlString, String jsonBody) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
+                String response = scanner.useDelimiter("\\A").next();
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
