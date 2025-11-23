bbd8a57adda1983b83f8d3ea24d79203e1956608
Vue-liang <2330983050@qq.com>
2025-11-23 21:45:13 +0800

    feat(array): 添加4个二分查找相关算法实现

--- leetcode-algorithm/src/main/java/com/liang/leetcode/array/BinarySearch.java
+++ leetcode-algorithm/src/main/java/com/liang/leetcode/array/BinarySearch.java
@@ -12,19 +12,20 @@ public class BinarySearch {
         int[] arr1 = {5, 7, 7, 8, 8, 10};
 //        System.out.println(getLeftBorder(arr1, 8) + 1);
 //        System.out.println(getRightBorder(arr1, 8) - 1);
-        System.out.println(Arrays.toString(searchRange(arr, 11)));
-        System.out.println(Arrays.toString(searchRange(arr1, 8)));
+//        System.out.println(Arrays.toString(searchRange(arr, 11)));
+//        System.out.println(Arrays.toString(searchRange(arr1, 8)));
+//        System.out.println("平方根取整" + mySqrt(9));
+//        System.out.println("平方根取整" + mySqrt(8));
+//        System.out.println("平方根取整" + mySqrt(4));
+        System.out.println("平方根取整" + isPerfectSquare(4));
+        System.out.println("平方根取整" + isPerfectSquare(8));
+        System.out.println("平方根取整" + isPerfectSquare(16));
     }
 
     /**
-     * 为什么直接return left；
-     * 因为如果上面的没有返回return middle，说明最后一定是，left>right
-     * 从而跳出循环的，在此之前是left=right，如果最后是right-1导致的left>right，
-     * 说明原来的right位置是大于target的，所以返回原来的right位置即left位置；
-     * 如果最后是left+1导致的left>right,说明是原来的的left=right这个位置小于target，
-     * 而right能移动到这个位置，说明此位置右侧是大于target的，
-     * left现在加1就移动到了这样的位置，返回left即可
+     * left>right 1. 上一个left+1 -> m<t 取完后 left > t 2. 上一个right-1 -> m>t 仍然如此
      */
+    // 35. 搜索插入位置
     public static int binarySearch(int[] arr, int target) {
         int len = arr.length;
         int left = 0;
@@ -47,6 +48,7 @@ public class BinarySearch {
      * 情况二：target 在数组范围中，且数组中不存在target，例如数组{3,6,7},target为5，此时应该返回{-1, -1}
      * 情况三：target 在数组范围中，且数组中存在target，例如数组{3,6,7},target为6，此时应该返回{1, 1}
      */
+    // 34. 查找元素的位置
     public static int[] searchRange(int[] arr, int target) {
         // 获取左边界
         int leftBorder = getLeftBorder(arr, target);
@@ -92,5 +94,61 @@ public class BinarySearch {
             }
         }
         return leftBoder;
+    }
+
+    // 获取的是>=target的第一个数的下标
+    // left>right 1. 上一个left+1 -> m<t 取完后 left >=t 2. 上一个right-1 -> m>=t 仍然如此
+    // 如果数组为空，或者所有数都 < target，则返回 nums.length
+    // 要求 nums 是非递减的，即 nums[i] <= nums[i + 1]
+    // 34. 查找元素的位置
+    public static int lowerBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] >= target) { // 将mid往左边逼近
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    // 69. x 的平方根
+    private static final int SQRT_INT_MAX = (int) Math.sqrt(Integer.MAX_VALUE);
+
+    public static int mySqrt(int x) {
+        int left = 0;
+        int right = Math.min(x, SQRT_INT_MAX);
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            int value = mid * mid;
+            if (value > x) {
+                right = mid - 1;
+            } else if (value < x) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return right; // v[r]<x
+    }
+    // 367. 有效的完全平方数
+    public static boolean isPerfectSquare(int num) {
+        int left = 0;
+        int right = Math.min(num, SQRT_INT_MAX);
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            int value = mid * mid;
+            if (value > num) {
+                right = mid - 1;
+            } else if (value < num) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
