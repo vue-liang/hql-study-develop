1a0705fe7a8e71c49bfe78c365b3f536e81619a9
Vue-liang <2330983050@qq.com>
2025-11-28 21:43:02 +0800

    feat(email): 添加邮箱发送功能及Markdown转HTML工具类

---
+++ leetcode-algorithm/src/main/java/com/liang/leetcode/array/MySort.java
@@ -0,0 +1,106 @@
+package com.liang.leetcode.array;
+
+import java.util.Arrays;
+
+public class MySort {
+    public static void main(String[] args) {
+        int[] arr = {5, 7, 6, 8, 9, 1, 2, 3, 4};
+//        bubbleSort(arr);
+//        insertSort(arr);
+//        selectSort(arr);
+//        quickSort(arr, 0, arr.length - 1);
+        quickSort1(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // 冒泡排序 左>右 不断交换 最大值在最右边
+    public static void bubbleSort(int[] arr) {
+        for (int j = arr.length; j > 0; j--) {
+            // 每一趟，最大值在最右边
+            for (int i = 0; i < j - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    // 插入排序 左<右 插入 找到插入位置 左边是已排好序的
+    public static void insertSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int t = arr[i + 1];
+            int j = i;
+            while (j >= 0) {
+                if (t < arr[j]) { // 前面的都是已排序好的
+                    arr[j + 1] = arr[j]; // 大的→移
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            arr[j + 1] = t;
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    // 选择排序 找到最小的 放到最左边
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int mIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[mIndex] > arr[j]) {
+                    mIndex = j;
+                }
+            }
+            int t = arr[mIndex];
+            arr[mIndex] = arr[i];
+            arr[i] = t;
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    // 快速排序 双指针法，保证 左<key  右>key
+    public static void quickSort(int[] arr, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }
+        int left = begin;
+        int right = end;
+        int key = begin;
+        while (left < right) {
+            while (arr[right] >= arr[key] && left < right) right--;
+            while (arr[left] <= arr[key] && left < right) left++;
+            swap(arr, left, right);
+        }
+        swap(arr, key, left);
+        key = left;
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr, begin, key - 1);
+        quickSort(arr, key + 1, end);
+    }
+
+    // 快速排序 挖坑法
+    public static void quickSort1(int[] arr, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }
+        int left = begin;
+        int right = end;
+        int key = arr[begin];
+        while (left < right) {
+            while (arr[right] >= key && left < right) right--;
+            arr[left] = arr[right];
+            while (arr[left] <= key && left < right) left++;
+            arr[right] = arr[left];
+        }
+        arr[left] = key;
+        quickSort1(arr, begin, left - 1);
+        quickSort1(arr, left + 1, end);
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int t = arr[a];
+        arr[a] = arr[b];
+        arr[b] = t;
+    }
+}
---
+++ spring-learn/src/test/java/com/liang/EmailTest.java
@@ -0,0 +1,107 @@
+package com.liang;
+
+import com.liang.utils.MarkdownConverter;
+import org.junit.jupiter.api.Test;
+
+import javax.activation.DataHandler;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class EmailTest {
+
+    @Test
+    public void testSendEmail() {
+        long start = System.currentTimeMillis();
+        //	账号信息
+        String username = "2330983050@qq.com";//	邮箱发送账号
+        String password = "";//	邮箱授权码
+
+        //	创建一个配置文件，并保存
+        Properties properties = new Properties();
+
+        //	SMTP服务器连接信息
+        //  126——smtp.126.com
+        //  163——smtp.163.com
+        //  qq——smtp.qq.com
+        properties.put("mail.smtp.host", "smtp.qq.com");//	SMTP主机名
+
+        //  126——25
+        //  163——465
+        properties.put("mail.smtp.port", "587");//	主机端口号
+        properties.put("mail.smtp.auth", "true");//	是否需要用户认证
+        properties.put("mail.smtp.starttls.enable", "true");//	启用TlS加密
+
+        // 获取默认session对象
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password); //发件人邮件用户名、授权码
+            }
+        });
+//        session.setDebug(true);
+
+        try {
+            //	创建邮件对象
+            MimeMessage message = new MimeMessage(session);
+            message.setSubject("测试发送主题");
+//            message.setText("测试发送文本信息");
+            message.setFrom(new InternetAddress(username, "你黄哥的测试邮件"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("2633630783@qq.com", "收件人别名")); //3512972187@qq.com
+
+//            Transport.send(message);
+            //	邮件主体
+            BodyPart textPart = new MimeBodyPart();
+
+            String htmlContent = MarkdownConverter.convertToHtml("**关于DDD是什么，在维基百科有一个明确的定义。\"Domain-driven design (DDD) is a major software design approach.\" 也就是说DDD是一种主要的软件设计方法。而软件设计涵盖了；范式、模型、框架、方法论。**\n" +
+                    "\n" +
+                    "- 范式（paradigm）指的是一种编程思想。\n" +
+                    "- 模型（model）指的是对现实世界或者问题的抽象描述。\n" +
+                    "- 框架（framework）指的是提供了一系列通用功能和结构的软件工具。\n" +
+                    "- 方法论（methodology）指的是一种系统的、有组织的解决问题的方法。\n" +
+                    "\n" +
+                    "所以，DDD不只是只有指导思想，伴随的DDD的还包括框架结构分层。但说到底，这些仍然是理论讨论。在没有一个DDD落地项目物参考下，其实大部分码农是没法完成DDD开发的。");
+            textPart.setContent(htmlContent, "text/html;charset=utf-8");
+
+            //	邮件附件
+            BodyPart filePart = new MimeBodyPart();
+            filePart.setFileName("测试邮箱附件");
+
+            //	提交附件文件
+            filePart.setDataHandler(new DataHandler(new ByteArrayDataSource(Files.readAllBytes(Paths.get("./src/test/java/files/test.txt")), "application/octet-stream")));
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(textPart);
+            multipart.addBodyPart(filePart);
+
+            //	将邮件装入信封
+            message.setContent(multipart);
+
+            Transport.send(message);
+            long end = System.currentTimeMillis();
+            System.out.println("发送邮箱耗时共计"+ TimeUnit.MILLISECONDS.toSeconds(end-start)+"s");
+        } catch (MessagingException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test_md2html() {
+        System.out.println(MarkdownConverter.convertToHtml("**关于DDD是什么，在维基百科有一个明确的定义。\"Domain-driven design (DDD) is a major software design approach.\" 也就是说DDD是一种主要的软件设计方法。而软件设计涵盖了；范式、模型、框架、方法论。**\n" +
+                "\n" +
+                "- 范式（paradigm）指的是一种编程思想。\n" +
+                "- 模型（model）指的是对现实世界或者问题的抽象描述。\n" +
+                "- 框架（framework）指的是提供了一系列通用功能和结构的软件工具。\n" +
+                "- 方法论（methodology）指的是一种系统的、有组织的解决问题的方法。\n" +
+                "\n" +
+                "所以，DDD不只是只有指导思想，伴随的DDD的还包括框架结构分层。但说到底，这些仍然是理论讨论。在没有一个DDD落地项目物参考下，其实大部分码农是没法完成DDD开发的。所以小傅哥今年花费了5个月假期/周末的时间，完成的《DDD简明开发教程》，帮助大家落地DDD编码。"));
+    }
+
+}
